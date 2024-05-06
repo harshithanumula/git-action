4d32a8d5964f2b9d0b02c302914fe31e72b04ac5
harshithanumula <165828712+harshithanumula@users.noreply.github.com>
2024-05-06 12:56:34 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("today topic is github actions"); 
+        System.out.println("today topic is github-actions"); 
     }
 }
